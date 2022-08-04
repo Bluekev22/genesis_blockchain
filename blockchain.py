296a568dd3011e581b2b7fe73ad67c565c0b3415
Kevin Shank <Kevin_Shank@yahoo.com>
2022-08-04 16:55:13 -0400

    get_previous_block

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,24 @@
-# Module 1 - Create a Blockchain
+#Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Building a blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+        def create_block(self, proof, previous_hash):
+            block = {'index': len(self.chain) + 1,
+                     'timestamp': str(datetime.datetime.now()),
+                     'proof': proof,
+                     'previous_hash': previous_hash}
+            self.chain.append(block)
+            return block
+        
+        def get_previous_block(self):
+            return self.chain[-1]
